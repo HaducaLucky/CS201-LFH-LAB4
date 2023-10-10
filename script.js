8c91c8514ba30491b24545f4c93c20979fa5b3b4
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2023-10-10 15:01:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let fruit = "banana";
+
+switch (fruit){
+    case "banana":
+         console.log("Banana is good!");
+         break;
+    case "orange":
+         console.log("I am not a fan of orange");
+         break;
+    case "Apple":
+        console.log("How you like them apples?");
+         break;
+    default:
+        console.log("I have never heard of that fruit.");
+        break;
+}
